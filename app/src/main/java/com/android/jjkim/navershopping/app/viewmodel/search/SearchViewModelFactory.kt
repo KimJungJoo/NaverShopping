5d3b1d6a6adcd6fb3c0a4ba86d1490601a44c1ad
JungJooKim <wildwaves76@gmail.com>
2022-12-30 14:36:45 +0900

    코드에러 수정

--- app/src/main/java/com/android/jjkim/navershopping/app/viewmodel/search/SearchViewModelFactory.kt
+++ app/src/main/java/com/android/jjkim/navershopping/app/viewmodel/search/SearchViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.android.jjkim.navershopping.app.service.repository.SearchRepository
 
 class SearchViewModelFactory constructor(private val searchRepository: SearchRepository): ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return if(modelClass.isAssignableFrom(SearchViewModel::class.java)) {
             SearchViewModel(this.searchRepository) as T
         } else {
